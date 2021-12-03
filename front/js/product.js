a059e74147e8f7d397ff0634185e5140b1bc013c
Asson Sarah <asson.sarah94@gmail.com>
2021-12-03 10:17:37 +0400

    Ajout dossier Front/back et js

---
+++ front/js/product.js
@@ -0,0 +1,15 @@
+
+
+
+
+function addCards(data) {
+     //création de la boucle pour l'affichage des produits 
+     for (product of data) {
+      // récupération dans le DOM du html correspondant à la carte d'un produit 
+      const item = document.getElementById("item")
+      //incrémentation des informations du produit dans le html (carte du produit) grâce à la classe product
+      ul.innerHTML ` 
+      <a href="./product.html?id=${product._id}">`
+     
+}
+}
